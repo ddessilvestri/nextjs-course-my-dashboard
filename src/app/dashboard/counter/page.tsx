0e5dd91ee63a4666b471f9a7332a219db8fb506b
daviddexteel <dsilvestri@dexteel.com>
2024-09-01 00:52:01 -0300

    first commit: cart counter

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,22 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+  title:'Shopping Cart',
+  description:'Check your cart products!'
+}
+
+
+export default function CounterPage(){
+
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>
+          Cart Products
+      </span>
+      
+      <CartCounter value={20}/>
+    </div>
+
+  )
+}
